183aeec96be3a26225f23dc175344766fb857685
ideen-ameer <138182387+ideen-ameer@users.noreply.github.com>
2023-06-30 08:51:58 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Booking from './components/USER/booking';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Booking/>
+    <App/>
   </React.StrictMode>
 );
 
